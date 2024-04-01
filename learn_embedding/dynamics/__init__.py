d4bc5ec108f8f5b8357c50e6786207d223a20ad4
Bbosc <baudouin.bosc@gmail.com>
2024-04-01 18:14:12 +0200

    reverting to initial state

--- learn_embedding/dynamics/__init__.py
+++ learn_embedding/dynamics/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from .first_geometry import FirstGeometry
-from .second_geometry import SecondGeometry, SecondGeometryRobot
+from .second_geometry import SecondGeometry
 from .linear_field import LinearField
 
 __all__ = ["FirstGeometry", "SecondGeometry", "LinearField"]

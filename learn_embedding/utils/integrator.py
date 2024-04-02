c583f078b765ed696c8b79ac0ad112dda8fe23d9
Baudouin Bosc <baudouin.bosc@gmail.com>
2024-04-02 17:40:55 +0200

    commented out function removed

--- learn_embedding/utils/integrator.py
+++ learn_embedding/utils/integrator.py
@@ -17,19 +17,6 @@ class Integrator():
 
         return x
 
-    # @staticmethod
-    # def second_order(dynamics, x0, T, dt):
-    #     dim = int(x0.shape[1]/2)
-    #     steps = int(T/dt)
-    #     x = torch.zeros(steps, x0.shape[0], x0.shape[1]).to(x0.device)
-    #     x[0, :, :] = x0
-
-    #     for i in range(steps-1):
-    #         x[i+1, :, dim:] = x[i, :, dim:] + dynamics(x[i, :, :])*dt
-    #         x[i+1, :, :dim] = x[i, :, :dim] + x[i+1, :, dim:]*dt
-
-    #     return x
-
     @staticmethod
     def second_order(dynamics, x0, T, dt):
         dim = int(x0.shape[1]/2)
@@ -42,4 +29,3 @@ class Integrator():
             x[i+1, :, :dim] = x[i, :, :dim] + x[i+1, :, dim:]*dt
 
         return x
-
